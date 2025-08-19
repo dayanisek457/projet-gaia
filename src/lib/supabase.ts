80d90c457bc30033e7f00a9c4c147924349d78a6
Yanis EL-KFEL <jorisliraud21@gmail.com>
2025-08-19 19:06:44 +0200

    Revert "Add Administrator Access with Supabase Authentication and S3 File Management"

--- src/lib/supabase.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://mvtlxvxywbdjkzcouyrn.supabase.co'
-const supabaseAnonKey = 'sb_secret_BnfP85MD-cRkChu0vA3gLQ_Y_nI-Egr'
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
